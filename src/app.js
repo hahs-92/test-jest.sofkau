5641dc77f84685edbc06008be989444f930020c4
Alex <alexandre_2420@outlook.com>
2022-03-14 14:46:45 -0500

    add app.js and math.js

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import { add, subtract, divide, multiply} from './math.js';
+
+export const doAdd      = (a, b) => add(a, b)
+
+export const doSubtract = (a, b) => subtract(a, b)
+
+export const doMultiply = (a, b) => multiply(a, b)
+
+export const doDivide   = (a, b) => divide(a, b)
