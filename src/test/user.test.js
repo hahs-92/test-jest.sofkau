d3ccfbc6f1faf101187ccb15078e3e13edfe3be7
Alex <alexandre_2420@outlook.com>
2022-03-18 19:25:26 -0500

    add user and user.test

---
+++ src/test/user.test.js
@@ -0,0 +1,14 @@
+const axios = require('axios')
+const Users = require('../user')
+
+
+jest.mock('axios');
+
+test('should fetch users', () => {
+    const users = [{name: 'Bob'}]
+    const resp = {data: users}
+
+    axios.get.mockResolvedValue(resp);
+
+    return Users.all().then(data => expect(data).toEqual(users));
+})
